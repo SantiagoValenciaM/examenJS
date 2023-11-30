ccb00e264b0c61fc7122c4aa0521dea84ae3d301
SantiagoValenciaM <svalencia7@ucol.mx>
2023-11-29 19:00:48 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// Mostrar en pantalla una lista desordenada de eventos que esten guardados en un array
+// Abajo de la lista, poner 2 botones, uno verde y otro rojo
+// El boton verde debe decir crear y el boton rojo eliminar
+
+// Array para almacenar los datos ingresados
+let dataArray = [];
+
+// Función para agregar un elemento al array y actualizar la lista
+function agregarEvento() {
+  let inputText = document.getElementById("inputText");
+  let Item = document.createElement("li");
+
+  if (inputText.value.trim() !== "") {
+    dataArray.push(inputText.value);
+    Item.appendChild(document.createTextNode(inputText.value));
+    document.getElementById("ulArray").appendChild(Item);
+    inputText.value = ""; // Limpiar el campo de entrada después de agregar
+  }
+}
+
+// Función para eliminar el último elemento del array y actualizar la lista
+function eliminarEvento() {
+  if (dataArray.length > 0) {
+    dataArray.pop(); // Eliminar el último elemento del array
+    var list = document.getElementById("ulArray");
+    list.removeChild(list.lastElementChild); // Eliminar el último elemento de la lista
+  }
+}
